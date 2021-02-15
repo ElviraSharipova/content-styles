6e81b4e8da54fca06970c2467944ab9a761980e4
GrigorievDmitry <36789339+GrigorievDmitry@users.noreply.github.com>
2021-02-15 21:52:29 +0300

    Initial

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
-import { ThemeProvider } from "@material-ui/styles";
-import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
-import { LayoutProvider } from "./context/LayoutContext";
-import { UserProvider } from "./context/UserContext";
-import { ManagementProvider } from "./context/ManagementContext";
-import {
-  ThemeProvider as ThemeChangeProvider,
-  ThemeStateContext
-} from "./context/ThemeContext";
-import { CssBaseline } from "@material-ui/core";
-import config from "../src/config";
-
-axios.defaults.baseURL = config.baseURLApi;
-axios.defaults.headers.common["Content-Type"] = "application/json";
-const token = localStorage.getItem("token");
-if (token) {
-  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <LayoutProvider>
-    <UserProvider>
-        <ThemeChangeProvider>
-          <ThemeStateContext.Consumer>
-            {theme => (
-              <ThemeProvider theme={theme}>
-                <ManagementProvider>
-                <CssBaseline />
-                <App />
-                </ManagementProvider>
-              </ThemeProvider>
-            )}
-          </ThemeStateContext.Consumer>
-        </ThemeChangeProvider>
-    </UserProvider>
-  </LayoutProvider>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
