c3063cf58c4c605ec1726704788dd5403be34368
elvira_sharipova <ellarox201@gmail.com>
2021-11-17 12:48:09 +0300

    add base styles

--- src/pages/modules/styles.js
+++ src/pages/modules/styles.js
@@ -1,121 +1,121 @@
 import { makeStyles } from "@material-ui/styles";
 
 export default makeStyles(theme => ({
-  paper: {
-    margin: "24px 0",
-    backgroundColor: "#f3f3f3",
-  },
-  virtualEnv: {
-    marginBottom: 64
-  },
-  test: {
-    marginBottom: "3%"
-  },
-  testButtons: {
-    marginTop: -12,
-    marginBottom: 48,
-  },
-  testHeader: {
-    padding: 36,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    maxWidth: 1280,
-  },
-  testCheckbox: {
-    paddingBottom: 6,
-    paddingTop: 6,
-  },
-  testCheckboxLabel: {
-    lineHeight: 1.3
-  },
-  codeEditor: {
-    margin: 27
-  },
-  progressBar: {
-    flexGrow: 1,
-    marginBottom: "6px",
-    backgroundColor: "rgba(0, 0, 0, 0.2)",
-  },
-  barColorPromary: {
-    backgroundColor: "#00C9FF"
-  },
-  dataStreamButton: {
-    margin: 24
-  },
-  checkmarkPrimary: {
-    color: "#00C9FF",
-    fontSize: 20,
-    marginRight: 10
-  },
-  checkmarkSecondary: {
-    color: "#00C9FF",
-    fontSize: 16,
-    marginRight: 10,
-    marginLeft: 18,
-  },
-  iconSecondary: {
-    fontSize: 20,
-    marginRight: 10
-  },
-  popover: {
-    pointerEvents: 'none',
-  },
-  paper: {
-    padding: theme.spacing(1),
-  },
-  contentText: {
-    "& > *": {
-      marginBottom: 12,
-      fontFamily: "Roboto"
+    paper: {
+        margin: "24px 0",
+        backgroundColor: "#f3f3f3",
     },
-    "& > p": {
-      marginBottom: 12,
-      marginTop: 12,
+    virtualEnv: {
+        marginBottom: 64
     },
-    "& p": {
-      marginBottom: 12,
-      marginTop: 12,
+    test: {
+        marginBottom: "3%"
     },
-    "& > .colored-frame": {
-      marginTop: 12,
-      marginBottom: 0,
-      paddingLeft: 24,
-      paddingRight: 12,
-      borderStyle: "solid",
-      borderRadius: 12,
-      borderWidth: 1,
-      borderColor: "#E0E0E0",
-      backgroundColor: "#F9F9FC",
+    testButtons: {
+        marginTop: -12,
+        marginBottom: 48,
     },
-    "& > .caption": {
-      marginBottom: 18,
-      marginTop: 6,
+    testHeader: {
+        padding: 36,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        maxWidth: 1280,
     },
-    "& ul": {
-      
-      marginLeft: 24,
-      marginTop: 6,
-      marginBottom: 18,
-      listStyleType: "disc",
+    testCheckbox: {
+        paddingBottom: 6,
+        paddingTop: 6,
     },
-    "& ol": {
-      marginLeft: 24,
-      marginTop: 6,
-      marginBottom: 18,
-      listStyleType: "decimal",
+    testCheckboxLabel: {
+        lineHeight: 1.3
     },
-    "& li": {
-      marginBottom: 6,
+    codeEditor: {
+        margin: 27
     },
-    "& h2": {
-      marginTop: 48,
-     
+    progressBar: {
+        flexGrow: 1,
+        marginBottom: "6px",
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
     },
-    "& a": {
-      target: "_blank",
-      rel: "noopener noreferrer",
+    barColorPromary: {
+        backgroundColor: "#00C9FF"
     },
-    
-  },
+    dataStreamButton: {
+        margin: 24
+    },
+    checkmarkPrimary: {
+        color: "#00C9FF",
+        fontSize: 20,
+        marginRight: 10
+    },
+    checkmarkSecondary: {
+        color: "#00C9FF",
+        fontSize: 16,
+        marginRight: 10,
+        marginLeft: 18,
+    },
+    iconSecondary: {
+        fontSize: 20,
+        marginRight: 10
+    },
+    popover: {
+        pointerEvents: 'none',
+    },
+    paper: {
+        padding: theme.spacing(1),
+    },
+    contentText: {
+        "& > *": {
+            marginBottom: 12,
+            fontFamily: "Roboto"
+        },
+        "& > p": {
+            marginBottom: 12,
+            marginTop: 12,
+        },
+        "& p": {
+            marginBottom: 12,
+            marginTop: 12,
+        },
+        "& > .colored-frame": {
+            marginTop: 12,
+            marginBottom: 0,
+            paddingLeft: 24,
+            paddingRight: 12,
+            borderStyle: "solid",
+            borderRadius: 12,
+            borderWidth: 1,
+            borderColor: "#E0E0E0",
+            backgroundColor: "#F9F9FC",
+        },
+        "& > .caption": {
+            marginBottom: 18,
+            marginTop: 6,
+        },
+        "& ul": {
+
+            marginLeft: 24,
+            marginTop: 6,
+            marginBottom: 18,
+            listStyleType: "disc",
+        },
+        "& ol": {
+            marginLeft: 24,
+            marginTop: 6,
+            marginBottom: 18,
+            listStyleType: "decimal",
+        },
+        "& li": {
+            marginBottom: 6,
+        },
+        "& h2": {
+            marginTop: 48,
+
+        },
+        "& a": {
+            target: "_blank",
+            rel: "noopener noreferrer",
+        },
+
+    },
 }));
